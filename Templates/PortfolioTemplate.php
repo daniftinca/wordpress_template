bea692092d1b474335863136f4621c594ed05a24
daniftinca <iftincadanalexandru@yahoo.com>
2018-08-06 21:17:50 +0300

    this

--- Templates/PortfolioTemplate.php
+++ Templates/PortfolioTemplate.php
@@ -7,58 +7,101 @@
  * Time: 2:40 PM
  */
 
-get_header("portfolio");
+get_header( "portfolio" );
 ?>
 
-<style>
-	.portfolio-content{
-		background-image: url("<?php
+    <style>
+        .heading-section {
+            background-image: url("<?php
 			echo
 			wp_get_attachment_url(
 				carbon_get_post_meta(
 					get_the_ID(),"heading_background_image"
 				))
 		?>");
-	}
+        }
 
-</style>
+    </style>
 
-<div class="portfolio-content">
+    <div class="portfolio-content">
 
-	<div class="heading-section">
-		<div class="heading-info">
-			<h1 class="heading-title">
+    <div class="heading-section">
+        <div class="heading-info">
+            <h1 class="heading-title">
 				<?php
-				echo carbon_get_post_meta(
-					get_the_ID(),"heading_title"
-				);
+				echo carbon_get_post_meta( get_the_ID(), "heading_title" );
 				?>
-			</h1>
-			<div class="heading-description">
+            </h1>
+            <div class="heading-description">
 				<?php
-				echo
-				apply_filters( 'the_content',
-					carbon_get_post_meta(
-					get_the_ID(),"heading_description"
-				));
+				echo apply_filters( 'the_content', carbon_get_post_meta( get_the_ID(), "heading_description" ) );
 				?>
-			</div>
-		</div>
-		<div class="heading-image">
-			<img src="<?php
-			echo
-			wp_get_attachment_url(
-				carbon_get_post_meta(
-					get_the_ID(),"heading_overlay_image"
-				));
-			?>" alt="" />
-		</div>
-	</div>
+            </div>
+        </div>
+        <div class="heading-image">
+            <img src="<?php
+			echo wp_get_attachment_url( carbon_get_post_meta( get_the_ID(), "heading_overlay_image" ) );
+			?>" alt=""/>
+        </div>
+    </div>
+    </div>
+    <div class="portfolio-item-section">
+		<?php
+
+		$items = carbon_get_post_meta( get_the_ID(), "contentwrapper" );
+		//print_r( $items );
+		foreach ( $items as $item ) {
+            if ( $item[_type] == "text_field" ) {
+            if ( $item["half_content"] ){
+            ?>
+
+                <div class="portfolio-item text-field half">
+                <?php
+                } else{
+                ?>
+                <div class="portfolio-item text-field">
+                    <?php } ?>
+                    <h2 class="item-title"><?php echo $item["title"] ?></h2>
+
+                    <div class="item-content">
+                        <?php
+                        echo apply_filters( 'the_content', $item["content"] );
+                        ?>
+                    </div>
+                </div>
+                <?php
+                } elseif ( $item[_type] == "images_field" ) {
+                        if ( $item["half_content"] ){
+            ?>
+
+            <div class="portfolio-item image-field half">
+                <?php
+                } else{
+                ?>
+                <div class="portfolio-item image-field">
+                    <?php }  ?>
+                    <h2 class="item-title"><?php echo $item["title"] ?></h2>
+
+                    <div class="item-content">
+		                <?php
+		                foreach ($item["content"] as $singleImage){
+		                    ?>
+                                <img src="<?php echo wp_get_attachment_url($singleImage["icon"]); ?>" alt="" />
+
+                            <?php
+                        }
+		                ?>
+                    </div>
+                </div>
+<!--             </div>-->
+    <?php
+                }
+			}
+			?>
 
 
 
-
-</div>
+    </div>
 
 
 <?php
--- custom-fields/portfolio.php
+++ custom-fields/portfolio.php
@@ -5,39 +5,34 @@
  * Date: 04-Aug-18
  * Time: 3:26 PM
  */
+
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-Container::make('post_meta','Portfolio Item')
-    ->where('post_template'	,'=', 'Templates/PortfolioTemplate.php')
-    ->add_tab(__('Header Section'),array(
-        Field::make('image','heading_background_image'),
-        Field::make('image','heading_overlay_image'),
-        Field::make('text','heading_title'),
-        Field::make('rich_text','heading_description'),
-    ))
-	->add_tab(__('Content Section'),array(
-		Field::make('text','content_title'),
-		Field::make('complex','contentwrapper')
-			->add_fields('text_field',array(
-				Field::make('text','title'),
-				Field::make('rich_text','content'),
-				Field::make('checkbox','half_content','Half Width?')
-					->set_option_value( 'yes' )
-			))
-			->add_fields('images_field',array(
-				Field::make('text','title'),
-				Field::make( 'complex', 'content' )
-				     ->add_fields( array(
-					     Field::make( 'image', 'icon' ),
-				     )),
-				Field::make('checkbox','half_content','Half Width?')
-				     ->set_option_value( 'yes' )
-			))
-			->add_fields('gallery_field',array(
-				Field::make('text','gallery_title')
-			)),
+Container::make( 'post_meta', 'Portfolio Item' )->where( 'post_template', '=', 'Templates/PortfolioTemplate.php' )->add_tab( __( 'Header Section' ), array(
+		Field::make( 'image', 'heading_background_image' ),
+		Field::make( 'image', 'heading_overlay_image' ),
+		Field::make( 'text', 'heading_title' ),
+		Field::make( 'rich_text', 'heading_description' ),
+	) )->add_tab( __( 'Content Section' ), array(
+		Field::make( 'text', 'content_title' ),
+		Field::make( 'complex', 'contentwrapper' )->add_fields( 'text_field', array(
+				Field::make( 'text', 'title' ),
+				Field::make( 'rich_text', 'content' ),
+				Field::make( 'checkbox', 'half_content', 'Half Width?' )->set_option_value( 'yes' )
+			) )->add_fields( 'images_field', array(
+				Field::make( 'text', 'title' ),
+				Field::make( 'complex', 'content' )->add_fields( array(
+						Field::make( 'image', 'icon' ),
+					) ),
+				Field::make( 'checkbox', 'half_content', 'Half Width?' )->set_option_value( 'yes' )
+			) )->add_fields( 'gallery_field', array(
+				Field::make( 'text', 'gallery_title' ),
+				Field::make( 'complex', 'gallery_content' )->add_fields( array(
+					Field::make( 'image', 'image' ),
+				) ),
+			) ),
 
-	));
+	) );
 
 
